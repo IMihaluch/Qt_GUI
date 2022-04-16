a0037fc0429ed0b16566075b1a713cfbd07bf804
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-04-16 04:21:52 +0300

    Create RU_in.ts

---
+++ RU_in.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation type="vanished">Главное окно</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="settings.cpp" line="225"/>
+        <source>Set Russian Language</source>
+        <translation>Установить английский язык</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="226"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="228"/>
+        <source>Hot Key &quot;Open&quot;</source>
+        <translation>Горячая клавиша &quot;Открыть&quot;</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="229"/>
+        <source>Hot Key &quot;Save&quot;</source>
+        <translation>Горячая клавиша &quot;Сохранить&quot;</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="230"/>
+        <source>Hot Key &quot;Clear Text Editor&quot;</source>
+        <translation>Горячая клавиша &quot;Очистить&quot;</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="231"/>
+        <source>Hot Key &quot;Exit&quot;</source>
+        <translation>Горячая клавиша &quot;Выход&quot;</translation>
+    </message>
+</context>
+<context>
+    <name>EditorTexts</name>
+    <message>
+        <location filename="editortexts.cpp" line="158"/>
+        <location filename="editortexts.cpp" line="160"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="editortexts.cpp" line="159"/>
+        <source>Open Read only</source>
+        <translation>Открыть для чтения</translation>
+    </message>
+    <message>
+        <location filename="editortexts.cpp" line="161"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="editortexts.cpp" line="142"/>
+        <location filename="editortexts.cpp" line="162"/>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="editortexts.cpp" line="163"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="editortexts.cpp" line="157"/>
+        <source>Text editor</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="editortexts.cpp" line="94"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="editortexts.cpp" line="119"/>
+        <source>Save file</source>
+        <translation>Сохранить файл</translation>
+    </message>
+</context>
+</TS>
