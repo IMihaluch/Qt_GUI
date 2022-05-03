2966286a809f7da0e5d3cac0e582cae09ce92cc1
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-05-03 13:30:38 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+
+#include "editortexts.h"
+#include <QApplication>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+   // Устанавливаем/выбираем стиль всего окна
+    //a.setStyle(QStyleFactory::create("windows"));
+    a.setStyle(QStyleFactory::create("fusion"));
+    //a.setStyle(QStyleFactory::create("macintosh"));
+    //a.setStyle(QStyleFactory::create("windowsvista"));
+    //a.setStyle(QStyleFactory::create("plastique"));
+    a.setWindowIcon(QIcon(":/Icons/main.png"));
+
+    EditorTexts w;
+    w.setMinimumHeight(200);
+    w.setMinimumWidth(300);
+    w.show();
+
+    return a.exec();
+}
